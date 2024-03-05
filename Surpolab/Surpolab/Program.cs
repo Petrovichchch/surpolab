98911303ba38ce01a56bb3338513ba55eff47a0c
Alice <alisa.denezhkina@yandex.ru>
2024-03-05 23:13:47 +0300

    Commit 1
    Add my preferences in music

---
+++ Surpolab/Surpolab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Surpolab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i love city-pop");
+        }
+    }
+}
